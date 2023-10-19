11cafffefeecfd93490c6c4fac205ab7a025ff57
Mateusz Majgier <mateusz.majgier@likims.com>
2023-10-19 17:53:19 +0200

    refactor: change all colors to vars, change px to rem

--- src/components/molecules/TaskForm/TaskForm.tsx
+++ src/components/molecules/TaskForm/TaskForm.tsx
@@ -9,18 +9,21 @@ type Props = {
   icon: React.ReactNode;
 };
 
+const TASK_ID = "task";
+
 export const TaskForm = ({ onFormSubmit, defaultValue, icon }: Props) => {
   const [isValid, setIsValid] = useState(false);
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    onFormSubmit(data.get("task") as string);
+    onFormSubmit(data.get(TASK_ID) as string);
     event.currentTarget.reset();
     setIsValid(false);
   };
   const isFormValid = (event: React.FormEvent<HTMLFormElement>) => {
     const data = new FormData(event.currentTarget);
-    const inputValue = data.get("task") as string;
+    const inputValue = data.get(TASK_ID) as string;
     if (inputValue.length > 0) {
       setIsValid(true);
     } else {
@@ -30,17 +33,17 @@ export const TaskForm = ({ onFormSubmit, defaultValue, icon }: Props) => {
 
   return (
     <form className="addForm" onSubmit={onSubmit} onChange={isFormValid}>
-      <label htmlFor="task" className="hiddenLabel">
+      <label htmlFor={TASK_ID} className="hiddenLabel">
         Task
       </label>
       <TextInput
-        id="task"
-        name="task"
+        id={TASK_ID}
+        name={TASK_ID}
         placeholder="Add a new task"
         defaultValue={defaultValue}
       />
       <Button
-        aria-label="task"
+        aria-label={TASK_ID}
         type="submit"
         colorType={isValid || defaultValue ? "success" : "disabled"}
         disabled={!isValid && !defaultValue}
